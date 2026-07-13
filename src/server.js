54163a2d3a885eeac76ddb50ef3b6e82891a0045
Sourabhkaushish <00sourabh52@gmail.com>
2026-07-13 17:11:18 +0530

    Fix server.js imports

--- src/server.js
+++ src/server.js
@@ -1,9 +1,51 @@
-//import dotenv from "dotenv";
-//dotenv.config();
+// //import dotenv from "dotenv";
+// //dotenv.config();
 
-//import app from "./app.js";
-//import connectDB from "./config/db.js";
-//import logger from "./utils/logger.js";
+// //import app from "./app.js";
+// //import connectDB from "./config/db.js";
+// //import logger from "./utils/logger.js";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import app from "./app.js";
+// import logger from "./utils/logger.js";
+
+// const PORT = process.env.PORT || 8888;
+
+// const startServer = async () => {
+//   try {
+
+//     app.listen(PORT, () => {
+//       logger.info(
+//         `Server running on http://localhost:${PORT}`
+//       );
+//     });
+
+//   } catch (error) {
+//     logger.error(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// startServer();
+// const PORT = process.env.PORT || 8888;
+
+// const startServer = async () => {
+//   try {
+//     await connectDB();
+
+//     app.listen(PORT, () => {
+//       logger.info(
+//         `Server running on http://localhost:${PORT}`
+//       );
+//     });
+//   } catch (error) {
+//     logger.error(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// startServer();
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,28 +54,8 @@ import logger from "./utils/logger.js";
 
 const PORT = process.env.PORT || 8888;
 
-const startServer = async () => {
+const startServer = () => {
   try {
-
-    app.listen(PORT, () => {
-      logger.info(
-        `Server running on http://localhost:${PORT}`
-      );
-    });
-
-  } catch (error) {
-    logger.error(error.message);
-    process.exit(1);
-  }
-};
-
-startServer();
-const PORT = process.env.PORT || 8888;
-
-const startServer = async () => {
-  try {
-    await connectDB();
-
     app.listen(PORT, () => {
       logger.info(
         `Server running on http://localhost:${PORT}`
