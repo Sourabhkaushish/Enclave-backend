6f10e086b1ddd3c4ee4dd69174297262d9881a8b
Sourabhkaushish <00sourabh52@gmail.com>
2026-07-13 16:56:30 +0530

    Fix duplicate app declaration

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 //import dotenv from "dotenv";
 //dotenv.config();
 
-import app from "./app.js";
-import connectDB from "./config/db.js";
-import logger from "./utils/logger.js";
+//import app from "./app.js";
+//import connectDB from "./config/db.js";
+//import logger from "./utils/logger.js";
 import dotenv from "dotenv";
 dotenv.config();
 
